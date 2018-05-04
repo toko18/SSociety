1b5d7b4a64957c42ba4dc044a61d0bcb91641653
toko18 <35262948+toko18@users.noreply.github.com>
2018-05-04 16:39:19 +0100

    iniciate project

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e0/406666f5af4f00181872a4de79f3cc4b
@@ -0,0 +1,9 @@
+package main;
+
+public class SSocietyClient
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
---
+++ SSocietyServer/src/main/SSocietyServer.java
@@ -0,0 +1,9 @@
+package main;
+
+public class SSocietyServer
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
