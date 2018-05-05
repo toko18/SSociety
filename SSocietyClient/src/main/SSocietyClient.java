ca6c9538fb13783b8e1bf87008e761ff249e2dae
Tomás Santiago <tomas.sant.mamede@protonmail.ch>
2018-05-05 16:13:35 +0100

    Set up done!

--- SSocietyClient/src/main/SSocietyClient.java
+++ SSocietyClient/src/main/SSocietyClient.java
@@ -5,6 +5,6 @@ public class SSocietyClient
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World!");
-		System.out.println("sup");
+		System.out.println("I have done it!");
 	}
 }
