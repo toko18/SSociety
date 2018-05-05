b2bf765a4443e658073c0684dfaa42c805f098c4
Tomás Santiago <tomas.sant.mamede@protonmail.ch>
2018-05-05 17:29:45 +0100

    First steps... :P

--- SSocietyClient/src/main/SSocietyClient.java
+++ SSocietyClient/src/main/SSocietyClient.java
@@ -1,10 +1,38 @@
 package main;
 
+import java.util.Scanner;
+
 public class SSocietyClient
 {
-	public static void main(String[] args)
+	
+	public static boolean login (String userName, String password)
 	{
-		System.out.println("Hello World!");
-		System.out.println("Verification");
+		Login client = new Login (userName, password);
+		return client.checkLogin();
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		Scanner userInput = new Scanner (System.in);
+		String userName = " ";
+		String password = " ";
+		
+		System.out.println("Welcome to SSociety!");
+		System.out.println("Enter your User Name: ");
+		System.out.println("Enter your password: ");
+		
+		while (true)
+		{
+			userName = userInput.next();
+			password = userInput.next();
+			
+			boolean success = login(userName, password);
+			
+			if (success)
+			{
+				break;
+			}
+		}
 	}
 }
