f3ec4b9c32fcc6d1e048cc69b8cf44d986012f9d
Tomás Santiago <tomas.sant.mamede@protonmail.ch>
2018-05-05 19:16:40 +0100

    User interface for login, registration help and exit

--- SSocietyClient/src/main/SSocietyClient.java
+++ SSocietyClient/src/main/SSocietyClient.java
@@ -4,12 +4,21 @@ import java.util.Scanner;
 
 public class SSocietyClient
 {
+	public static void helpScreen(Scanner userInput)
+	{
+		//open help file
+		
+		System.out.print("Press Enter to continue...");
+		
+		String nextStep;
+		nextStep = userInput.next();
+	}
 	
-	public static void login (Scanner userInput)
-	{	
+	public static void registrationScreen(Scanner userInput)
+	{
 		int chosenOption = 0;
 		
-		while (chosenOption == 1 || chosenOption == 2)
+		while (chosenOption != 1 || chosenOption != 2)
 		{
 			System.out.println("1 - Back");
 			System.out.println("2 - Continue");
@@ -23,34 +32,127 @@ public class SSocietyClient
 			}
 			else if (chosenOption == 2)
 			{	
-				String userName = " ";
-				String password = " ";
+				String userName;
+				String firstPassword;
+				String secondPassword;
 				
-				System.out.print("Enter your username: ");
-				userName = userInput.next();
-				System.out.print("Enter your password: ");
-				System.out.println();
+				int success = 0;
+				
+				while(success == 0)
+				{
+					System.out.print("Choose your username: ");
+					userName = userInput.next();
+					System.out.print("Choose your password: ");
+					firstPassword = userInput.next();
+					System.out.println("Enter chosen password again:");
+					secondPassword = userInput.next();
+					
+					Registration userRegistration = new Registration(userName, firstPassword, secondPassword);
+					
+					String nextStep;
+					
+					if (userRegistration.check() == 0) 
+					{
+						success = 1;
+						System.out.println("Success! Welcome to SSociety!");
+						System.out.println("Press Enter to continue...");
+						
+						nextStep = userInput.next();
+						firstScreen(userInput);
+						
+					}
+					else if (userRegistration.check() == 1)
+					{
+						System.out.println("A user with this name already exists...");
+						System.out.println("Try again");
+						System.out.println();
+						System.out.println("Press Enter key to continue...");
+						
+						nextStep = userInput.next();
+						//clear screen
+					}
+					else if (userRegistration.check() == 2)
+					{
+						System.out.println("Entered passwords don't match");
+						System.out.print("Press Enter key to continue...");
+						
+						nextStep = userInput.next();
+						//clear screen
+					}
+				}
 			}
 			//clean screen
 		}
 	}
 	
-	public static void firstScreen(Scanner userInput)
-	{
-		System.out.println("Welcome to SSociety!");
-		System.out.println();
-		System.out.println("1 - Login");
-		System.out.println("2 - Register");
-		System.out.println("3 - Help");
-		System.out.println("4 - Exit");
-		
+	public static void loginScreen (Scanner userInput)
+
+	{	
 		int chosenOption = 0;
-		chosenOption = userInput.nextInt();
 		
-		if(chosenOption == 1) 
+		while (chosenOption != 1 || chosenOption != 2)
 		{
+			System.out.println("1 - Back");
+			System.out.println("2 - Continue");
+			
+			System.out.print("Enter option: ");
+			chosenOption = userInput.nextInt();
+			
+			if(chosenOption == 1)
+			{
+				firstScreen(userInput);
+			}
+			else if (chosenOption == 2)
+			{	
+				String userName;
+				String password;
+				
+				System.out.print("Enter your username: ");
+				userName = userInput.next();
+				System.out.print("Enter your password: ");
+				password = userInput.next();
+				
+				Login userLogin = new Login(userName, password);
+			}
 			//clean screen
-			login(userInput);
+		}
+	}
+	
+	
+	public static void firstScreen(Scanner userInput)
+	{	
+		int chosenOption = 0;
+		
+		while(chosenOption != 1 || chosenOption != 2 || chosenOption != 3 || chosenOption != 4)
+		{
+			System.out.println("Welcome to SSociety!");
+			System.out.println();
+			System.out.println("1 - Login");
+			System.out.println("2 - Register");
+			System.out.println("3 - Help");
+			System.out.println("4 - Exit");
+			
+			chosenOption = userInput.nextInt();
+			
+			if(chosenOption == 1) 
+			{
+				//clean screen
+				loginScreen(userInput);
+			}
+			else if(chosenOption == 2) {
+				//clean screen
+				registrationScreen(userInput);
+			}
+			else if(chosenOption == 3)
+			{
+				helpScreen(userInput);
+				//cat file with help instruction (vais fazer esta merda paneleiro) --> toko18
+			}
+			else if(chosenOption == 4)
+			{
+				System.exit(0);
+			}
+			//clean screen
 		}
 	}
 	
