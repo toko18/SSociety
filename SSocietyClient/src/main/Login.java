7c97691e95ffe482e78d7e534f2ca81e55b2deb5
Tomás Santiago <tomas.sant.mamede@protonmail.ch>
2018-05-05 17:31:15 +0100

    First steps... :P

---
+++ SSocietyClient/src/main/Login.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Login {
+	private String userName;
+	private String password;
+	
+	Login (String u, String p)
+	{
+		userName = u;
+		password = p;
+	}
+	
+	public boolean checkLogin(String userName, String password)
+	{
+		System.out.println("This user is not registered!");
+		System.out.println("Try again or register.");
+		
+		return 
+	}
+	
+
+}
