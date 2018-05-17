40df2c3fdb77820614b2df4324f34b224dd32c00
Tomás Santiago <tomas.sant.mamede@protonmail.ch>
2018-05-17 10:31:30 +0100

    Account Settings started - Missing deleteAcoount() method. class topic concluded

--- SSocietyClient/src/settings/AccountSettings.java
+++ SSocietyClient/src/settings/AccountSettings.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import logged.Home;
 
 import java.io.FileWriter;
-//import java.io.FileReader;
-//import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.Console;
 
